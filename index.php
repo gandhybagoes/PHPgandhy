c29f96bfdfae77f6fb4a755c3510c197900b23e5
gandhy <gbagoescahyono@gmail.com>
2016-08-03 16:02:53 +0700

    acak

--- index.php
+++ index.php
@@ -4,18 +4,9 @@
 	<title>Belajar Git</title>
 </head>
 <body>
-<<<<<<< HEAD
 
-	<?php
-	
-	echo "hello world";	
+	<h1>Beon</h1>
+	<p>sdkgvfbdjshfcbgskdchbsdukfjb</p>
 
-	?>
-
-=======
-	<h1>Hello Git</h1>
-	<p>ini paragraf</p>
-	<p>ini paragraf2</p>
->>>>>>> 1706499db47b64bb2415e34f9f408bdabfb19892
 </body>
 </html>
